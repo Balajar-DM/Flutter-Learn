c60749b6067a2d20f3fecb3a2eaada236eaeddd2
DionMontolalu <dionmontolalu@gmail.com>
2020-09-02 11:44:15 +0800

    Latihan Container

--- lib/main.dart
+++ lib/main.dart
@@ -8,22 +8,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan Row dan Column"),
+          title: Text("Latihan Container"),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Text 1"),
-            Text("Text 2"),
-            Text("Text 3"),
-            Row(
-              children: <Widget>[
-                Text("Text 4"),
-                Text("Text 5"),
-                Text("Text 6"),
-              ],
-            )
-          ],
+        body: Container(
+          color: Colors.red,
+          margin: EdgeInsets.fromLTRB(10, 15, 20, 25),
+          padding: EdgeInsets.only(bottom: 20, top: 20),
+          child: Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient:
+                  LinearGradient(colors: <Color>[Colors.amber, Colors.blue]),
+            ),
+          ),
         ),
       ),
     );
