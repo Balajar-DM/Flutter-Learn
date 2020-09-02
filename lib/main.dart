537bdf05deda9f797f8e27241e687c8393cfc4ef
DionMontolalu <dionmontolalu@gmail.com>
2020-09-02 14:56:55 +0800

    Stateful Widget Demo

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 0;
+  void tekanTombol() {
+    setState(() {
+      number = number + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan Flutter"),
+          title: Text("Stateful Widget Demo"),
         ),
-        body: Center(child: Text("Belajar Fluter")),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              number.toString(),
+              style: TextStyle(fontSize: 10 + number.toDouble()),
+            ),
+            RaisedButton(
+              child: Text("Tambah Bilangan"),
+              onPressed: tekanTombol,
+            )
+          ],
+        )),
       ),
     );
   }
