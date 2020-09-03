4acaf1127c1fc0b2b00df76c949da5182adf4a58
DionMontolalu <dionmontolalu@gmail.com>
2020-09-03 13:13:39 +0800

    Latihan ListView

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,55 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> widgets = [];
+  int counter = 1;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Flutter"),
-        ),
-        body: Center(child: Text("Belajar Fluter")),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Latihan ListView"),
       ),
-    );
+      body: ListView(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              RaisedButton(
+                  child: Text("Tambah Data"),
+                  onPressed: () {
+                    setState(() {
+                      widgets.add(Text(
+                        "Data ke-" + counter.toString(),
+                        style: TextStyle(fontSize: 30),
+                      ));
+                      counter++;
+                    });
+                  }),
+              RaisedButton(
+                child: Text("Hapus"),
+                onPressed: () {
+                  setState(() {
+                    widgets.removeLast();
+                    counter--;
+                  });
+                },
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: widgets,
+          ),
+        ],
+      ),
+    ));
   }
 }
