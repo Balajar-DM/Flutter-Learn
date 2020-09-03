1997526f35c55077a30fdb5669f7b589d1f54532
DionMontolalu <dionmontolalu@gmail.com>
2020-09-03 10:06:28 +0800

    Master code

--- lib/main.dart
+++ lib/main.dart
@@ -2,48 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String message = "Ini adalah Text";
-
-  /*Method biasa
-  void tombolDitekan() {
-    setState(() {
-      message = "Tombol ini sudah ditekan";
-    });
-  }*/
-
-  /*Anonymous method = Method yang tidak memiliki nama
-  () { kode_program }
-  Digunakan untuk satu kali saja atau tidak digunakan di tempat lain*/
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Anonymous Method"),
+          title: Text("Latihan Flutter"),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(message),
-              RaisedButton(
-                  child: Text("Tekan Saya"),
-                  //Penggunaan anonymous method
-                  onPressed: () {
-                    setState(() {
-                      message = "Tombol ini sudah ditekan";
-                    });
-                  }),
-            ],
-          ),
-        ),
+        body: Center(child: Text("Belajar Fluter")),
       ),
     );
   }
