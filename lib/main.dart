1a702e391868499265f696dff7be5837841a4a35
DionMontolalu <dionmontolalu@gmail.com>
2020-09-07 19:11:41 +0800

    Flexible Layout Widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,52 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan Flutter"),
+          title: Text("Flexible Layout"),
         ),
-        body: Center(child: Text("Belajar Fluter")),
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.red,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.green,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.purple,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 2,
+              child: Container(
+                color: Colors.amber,
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                color: Colors.blueAccent,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
