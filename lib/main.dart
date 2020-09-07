327293325d7455ad8f6215497ac4ed29b4cdbf2f
DionMontolalu <dionmontolalu@gmail.com>
2020-09-07 21:43:49 +0800

    Latihan Spacer Widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,42 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan Flutter"),
+          title: Text("Spacer Widget"),
         ),
-        body: Center(child: Text("Belajar Fluter")),
+        body: Center(
+          child: Row(
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Spacer(
+                flex: 1,
+              ),
+              Container(
+                width: 80,
+                height: 80,
+                color: Colors.red,
+              ),
+              Spacer(
+                flex: 2,
+              ),
+              Container(
+                width: 80,
+                height: 80,
+                color: Colors.green,
+              ),
+              Spacer(
+                flex: 3,
+              ),
+              Container(
+                width: 80,
+                height: 80,
+                color: Colors.blue,
+              ),
+              Spacer(
+                flex: 2,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
