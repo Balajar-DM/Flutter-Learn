a860091d10a3bc58b1bb0f94489a6e015ae43ff5
DionMontolalu <dionmontolalu@gmail.com>
2021-02-08 06:58:12 +0800

    Navigasi Multi Page Screen

---
+++ lib/second_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Back"),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
