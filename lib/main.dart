22797ef84e2237fedddf843371cd0f5ef4edfac1
DionMontolalu <dionmontolalu@gmail.com>
2021-02-08 11:12:01 +0800

    Latihan TextField

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan Flutter"),
+          title: Text("Latihan Text Field"),
         ),
-        body: Center(child: Text("Belajar Fluter")),
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              TextField(
+                obscureText: true,
+                maxLength: 5,
+                maxLines: 1,
+                onChanged: (value) {
+                  setState(() {});
+                },
+                controller: controller,
+              ),
+              Text(controller.text),
+            ],
+          ),
+        ),
       ),
     );
   }
