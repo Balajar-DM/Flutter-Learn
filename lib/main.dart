e3a68a37c40941e6c103a0a910e9080713a07073
DionMontolalu <dionmontolalu@gmail.com>
2021-02-08 10:06:57 +0800

    Latihan custom AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan Flutter"),
+          leading: Icon(
+            Icons.adb,
+            color: Colors.white,
+          ),
+          title: Text(
+            "Appbar Example",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: () {},
+            ),
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xff0096ff), Color(0xff6610f2)],
+                begin: FractionalOffset.topLeft,
+                end: FractionalOffset.bottomRight,
+              ),
+
+              //Decoration Image sengaja kita kase ilang
+              /*image: DecorationImage(
+                  image: AssetImage("assets/corak.png"),
+                  fit: BoxFit.none,
+                  repeat: ImageRepeat.noRepeat),*/
+            ),
+          ),
         ),
-        body: Center(child: Text("Belajar Fluter")),
       ),
     );
   }
