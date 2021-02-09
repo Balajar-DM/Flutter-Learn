2d4d4984c98a82748b7ff58c47da45225e0e9243
DionMontolalu <dionmontolalu@gmail.com>
2021-02-10 01:10:49 +0800

    Latihan MediaQuery

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Flutter"),
-        ),
-        body: Center(child: Text("Belajar Fluter")),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Latihan MediaQuery"),
+      ),
+      body: Container(
+        color: Colors.red,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height / 2,
       ),
     );
   }
