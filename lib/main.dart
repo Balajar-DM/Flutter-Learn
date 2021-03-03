f850f98690508733314ac344e6664d8fcf7240c8
DionMontolalu <dionmontolalu@gmail.com>
2021-03-03 19:31:52 +0800

    Latihan TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Flutter"),
-        ),
-        body: Center(child: Text("Belajar Fluter")),
-      ),
+      home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Contoh Tabbar"),
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Tab(
+                    icon: Icon(Icons.comment),
+                    text: "Comment",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.ac_unit),
+                    text: "Ac Unit",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.access_alarm),
+                    text: "Alarm",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.accessibility),
+                    text: "Access",
+                  ),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: [
+                Center(
+                  child: Text("Tabbar 1"),
+                ),
+                Center(
+                  child: Text("Tabbar 2"),
+                ),
+                Center(
+                  child: Text("Tabbar 3"),
+                ),
+                Center(
+                  child: Text("Tabbar 4"),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
