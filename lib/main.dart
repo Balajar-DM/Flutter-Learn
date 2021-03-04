c3dc15ce09df2e25bf34d8544be9b28dc3215ed6
DionMontolalu <dionmontolalu@gmail.com>
2021-03-04 17:17:22 +0800

    Style TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -5,50 +5,52 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TabBar myTabBar = TabBar(
+      indicator: BoxDecoration(
+          color: Colors.red,
+          border: Border(
+              top: BorderSide(
+            color: Colors.purple,
+            width: 5,
+          ))),
+      tabs: <Widget>[
+        Tab(
+          icon: Icon(Icons.comment),
+          text: "Comment",
+        ),
+        Tab(
+          icon: Icon(Icons.ac_unit),
+          text: "Ac Unit",
+        ),
+      ],
+    );
+
     return MaterialApp(
       home: DefaultTabController(
-          length: 4,
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text("Contoh Tabbar"),
-              bottom: TabBar(
-                tabs: <Widget>[
-                  Tab(
-                    icon: Icon(Icons.comment),
-                    text: "Comment",
-                  ),
-                  Tab(
-                    icon: Icon(Icons.ac_unit),
-                    text: "Ac Unit",
-                  ),
-                  Tab(
-                    icon: Icon(Icons.access_alarm),
-                    text: "Alarm",
-                  ),
-                  Tab(
-                    icon: Icon(Icons.accessibility),
-                    text: "Access",
-                  ),
-                ],
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Tabbar Style"),
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
+              child: Container(
+                color: Colors.brown,
+                child: myTabBar,
               ),
             ),
-            body: TabBarView(
-              children: [
-                Center(
-                  child: Text("Tabbar 1"),
-                ),
-                Center(
-                  child: Text("Tabbar 2"),
-                ),
-                Center(
-                  child: Text("Tabbar 3"),
-                ),
-                Center(
-                  child: Text("Tabbar 4"),
-                ),
-              ],
-            ),
-          )),
+          ),
+          body: TabBarView(
+            children: [
+              Center(
+                child: Text("Tabbar 1 ku"),
+              ),
+              Center(
+                child: Text("Tabbar 2 ku"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
