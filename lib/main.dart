284a0a8edf1183dd7c7f46eb17b66cd4209bad23
DionMontolalu <dionmontolalu@gmail.com>
2021-04-21 18:46:18 +0800

    Latihan QRCode Scan

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Flutter"),
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  String judul = "Hasil QR Code";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Scan"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              judul,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                judul = await scanner.scan();
+                setState(() {});
+              },
+              child: Text("Scan QR"),
+            ),
+          ],
         ),
-        body: Center(child: Text("Belajar Fluter")),
       ),
     );
   }
